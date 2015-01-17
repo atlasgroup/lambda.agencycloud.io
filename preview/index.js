808c2a16cd24deb92a003e2b04b5fc0d3209cb5a
R.V.d.M <r@rvdm.la>
2015-01-16 18:04:36 -0800

    Updated comment

--- preview/index.js
+++ preview/index.js
@@ -2,9 +2,9 @@ var socket = require( 'socket.io-client' )( 'https://api.agencycloud.io:443' );
 
 exports.handler = function( event , context ) {
 
-  socket.emit( 'clean' , event.Records[ 0 ].s3.object.key , function() {
+  socket.emit( 'preview' , event.Records[ 0 ].s3.object.key , function() {
 
-    console.log( 'Done!' );
+    console.log( 'Done cleaning up preview.' );
 
     context.done();
 
